85abebf4f9843bff603abcf1d571f9ab35dc4f9a
publico <publico@infor37b06.alu.unq.edu.ar>
2023-09-28 18:45:42 -0300

    Ejercicio 02

---
+++ ejercicios.stream/src/ejercicios/stream/ejercicio02/Ejercicio02.java
@@ -0,0 +1,20 @@
+package ejercicios.stream.ejercicio02;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ejercicio02
+{
+	public boolean algunoCumple( List<String> lista, String prefijo )
+	{
+		boolean encontre = false;
+		int i = 0;
+		
+		while( !encontre && i < lista.size() )
+		{
+			encontre = lista.get(i).startsWith(prefijo);
+			i++;
+		}
+		return encontre;
+	}
+	
+}
