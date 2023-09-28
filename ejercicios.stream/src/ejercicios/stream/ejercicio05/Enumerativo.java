ed67f68386d7a77b8961172341fc47b380eda7cd
publico <publico@infor37b06.alu.unq.edu.ar>
2023-09-28 20:27:34 -0300

    asd

---
+++ ejercicios.stream/src/ejercicios/stream/ejercicio05/Enumerativo.java
@@ -0,0 +1,18 @@
+package ejercicios.stream.ejercicio05;
+
+public class Enumerativo
+{
+	public enum etapaHeridas
+	{
+		ROJO(7), GRIS(5), AMARILLO(2), MIEL(1);
+		
+		private int nivelDeRiesgo;
+		
+		
+		
+		
+		
+	}
+	
+	
+}
